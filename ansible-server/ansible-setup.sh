3b154d6679ce8fecda15d6885211ceaa2dc23ac3
Imrane Akkouh <akkouh.imrane@gmail.com>
2022-10-13 12:54:10 +0100

    centos8 machine with ansible installed

---
+++ ansible-server/ansible-setup.sh
@@ -0,0 +1,7 @@
+cd /etc/yum.repos.d/
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+sudo yum update -y
+sudo yum install python3-pip -y
+sudo pip3 install --upgrade pip
+sudo pip3 install ansible
